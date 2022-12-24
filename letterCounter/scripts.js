98b93156c84f4efc00e3bdb8581869d7467844df
salawow <rodrigosalazar5@hotmail.com>
2022-12-24 19:47:22 -0300

    letterCounter more style

--- letterCounter/scripts.js
+++ letterCounter/scripts.js
@@ -1,5 +1,16 @@
 let input = document.getElementById('inp');
+let count = document.getElementById('count');
 
 input.addEventListener('keyup', () => {
-    document.getElementById("count").innerHTML = "Count :" + " " + input.value.length;
+    let textCount = input.value.match(/\S/g).length;
+    count.innerText = `The phrase "${input.value}" has >> ${textCount} << letters`;
 });
+
+/*const initRestart = () => {
+    let restart = input.value.length;
+    if (restart == 1) {
+        restart == 'The phrase "" has >> 0 << letters';
+    }
+    return restart;
+};
+*/
